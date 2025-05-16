4d0f9f760d7b47a09d5027514e8c63cc098b90ca
nikolay <nikolay-kmc@mail.ru>
2025-05-17 00:06:46 +0300

    patterns

--- src/test/java/test/DeliveryTest.java
+++ src/test/java/test/DeliveryTest.java
@@ -1,57 +1,51 @@
-package tests;
+package test;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.*;
 import data.DataGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import java.time.Duration;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryTest {
 
     @BeforeEach
-    void setup() {
+    public void setUp() {
         open("http://localhost:9999");
     }
 
     @Test
-    @DisplayName("Should successful plan and replan meeting")
-    void shouldSuccessfulPlanAndReplanMeeting() {
-        Configuration.holdBrowserOpen = true;
-        var validUser = DataGenerator.Registration.generateUser("ru");
-        var daysToAddForFirstMeeting = 6;
-        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-        var daysToAddForSecondMeeting = 7;
-        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+    public void shouldSendAndReplanDateTest() {
 
-        $("[data-test-id='city'] input").sendKeys(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(firstMeetingDate);
-        $("[data-test-id='name'] input").sendKeys(validUser.getName());
-        $("[data-test-id='phone'] input").sendKeys(DataGenerator.generatePhone("ru"));
-        $("[data-test-id='agreement']").click();
-        $$("button").findBy(text("Запланировать")).click();
+        var user = DataGenerator.Registration.generateUser("ru");
+        var firstMeetingDate = DataGenerator.generateDate(4);
+        var secondMeetingDate = DataGenerator.generateDate(7);
+
+        $("[data-test-id='city'] input.input__control").setValue(user.getCity());
+        $("[data-test-id='date'] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input.input__control").setValue(firstMeetingDate);
+        $("[data-test-id='name'] input.input__control").setValue(user.getName());
+        $("[data-test-id='phone'] input.input__control").setValue(user.getPhone());
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $("button.button").click();
 
         $("[data-test-id='success-notification']")
-                .shouldHave(Condition.text("Успешно!"))
-                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate))
-                .shouldHave(visible);
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text(firstMeetingDate));
 
-        $("[data-test-id='date'] input").doubleClick().sendKeys(secondMeetingDate);
-        $$("button").findBy(text("Запланировать")).click();
+        $("[data-test-id='date'] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input.input__control").setValue(secondMeetingDate);
+        $("button.button").click();
         $("[data-test-id='replan-notification']")
-                .shouldHave(Condition.text("Необходимо подтверждение"))
-                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
-                .shouldHave(visible);
+                .shouldBe(Condition.visible,Duration.ofSeconds(3));
 
-        $$("button").findBy(text("Перепланировать")).click();
+        $("[data-test-id='replan-notification'] button.button").click();
         $("[data-test-id='success-notification']")
-                .shouldHave(Condition.text("Успешно!"))
-                .shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate))
-                .shouldHave(visible);
-
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text(secondMeetingDate));
     }
-
 }
