b7a4408234cb2f8548b952a25ed647f2e531584a
nikolay <nikolay-kmc@mail.ru>
2025-05-17 17:19:48 +0300

    pattern3I

--- src/test/java/test/DeliveryTest.java
+++ src/test/java/test/DeliveryTest.java
@@ -1,51 +1,62 @@
 package test;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
 import data.DataGenerator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryTest {
 
     @BeforeEach
-    public void setUp() {
+    public void setup() {
         open("http://localhost:9999");
     }
 
     @Test
-    public void shouldSendAndReplanDateTest() {
+    @DisplayName("Should successful plan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+       var validUser = DataGenerator.Registration.generateUser("ru");
 
-        var user = DataGenerator.Registration.generateUser("ru");
+
         var firstMeetingDate = DataGenerator.generateDate(4);
         var secondMeetingDate = DataGenerator.generateDate(7);
 
-        $("[data-test-id='city'] input.input__control").setValue(user.getCity());
-        $("[data-test-id='date'] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input.input__control").setValue(firstMeetingDate);
-        $("[data-test-id='name'] input.input__control").setValue(user.getName());
-        $("[data-test-id='phone'] input.input__control").setValue(user.getPhone());
-        $("[data-test-id='agreement'] .checkbox__box").click();
-        $("button.button").click();
 
-        $("[data-test-id='success-notification']")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.text(firstMeetingDate));
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(Selectors.byText("Запланировать")).click();
+        $(Selectors.withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(visible);
 
-        $("[data-test-id='date'] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input.input__control").setValue(secondMeetingDate);
-        $("button.button").click();
-        $("[data-test-id='replan-notification']")
-                .shouldBe(Condition.visible,Duration.ofSeconds(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(secondMeetingDate);
+        $(Selectors.byText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(exactText("У вас уже заплонирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(visible);
 
-        $("[data-test-id='replan-notification'] button.button").click();
-        $("[data-test-id='success-notification']")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.text(secondMeetingDate));
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
+                .shouldBe(visible);
     }
 }
+
+
+
+
